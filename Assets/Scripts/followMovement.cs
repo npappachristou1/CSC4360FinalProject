1f41924e0d43ef2c2696f8ff8ce9da49d3d41db9
Gerald Guerrero <sbgeraldg@gmail.com>
2022-11-11 12:42:02 -0500

    added location data that can transfer between scenes

--- Assets/Scripts/followMovement.cs
+++ Assets/Scripts/followMovement.cs
@@ -14,8 +14,7 @@ public class followMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        origPos = transform.position;
-        targetPos = character.transform.position;
+        
     }
 
     // Update is called once per frame
